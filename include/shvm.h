030e1d1c7309cb6f36dd12781f6c82dcd74751da
uael <lucas.abel@yandex.com>
2018-02-16 18:11:59 +0100

    project structure

---
+++ include/shvm.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shvm.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/07 09:52:30 by alucas-           #+#    #+#             */
+/*   Updated: 2017/11/23 17:28:28 by alucas-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHVM_H
+# define SHVM_H
+
+# include "shvm/bi.h"
+# include "shvm/env.h"
+# include "shvm/job.h"
+# include "shvm/op.h"
+# include "shvm/proc.h"
+# include "shvm/var.h"
+# include "shvm/vm.h"
+
+#endif
